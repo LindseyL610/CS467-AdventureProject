8dc68c40983a48fc74774a30c9df0addd62b664c
1015\Jason DiMedio <dimedioj@oregonstate.edu>
2019-12-05 15:59:14 -0500

    Test solution.

--- Utilities.py
+++ Utilities.py
@@ -38,6 +38,8 @@ COLORS['<CLUE>'] = COLORS['<red>']
 def say(text):
 	#if there is no string data, do nothing
 	if text is not None and (text == "") is False and isinstance(text, str):
+
+		
 		text = text + "</>"
 		for color in COLORS:
 			#Color only supported on Linux/Mac
@@ -47,6 +49,14 @@ def say(text):
 				text = text.replace(color, "")
 
 		print(WRAPPER.fill(text))
+
+	print()
+	print("WRAPPER.fill(text) (no color processing):")
+	print(WRAPPER.fill(text))
+	
+	print()
+	print("print(text):")
+	print(text)
 
 def list_to_words(object_list):
   list_length = len(object_list)
@@ -94,4 +104,71 @@ def find_by_name(name, group_to_search):
 #     if thing.name == name:
 #       return thing
 #   return None
+TEST_NEUTRAL = "Notes on the Balcony\nEnter the tower at your own risk, you may find yourself caught in an infinite loop. To remedy this, you must discover who you are. As with most journeys, you may want to start with a simple phrase: \"Hello World.\""
 
+TEST = "<WRITTEN_TEXT>Notes on the Balcony\nEnter the <green>tower</> at your own risk, you may find yourself caught in an infinite loop. To remedy this, you must discover who you are. As with most journeys, you may want to start with a simple phrase: \"Hello World.\"</>"
+
+text = TEST.split('\n')
+print(text)
+
+for line in text:
+	#print(line)
+	line_cpy = line
+	
+	SPECIAL_CHAR = '`'
+	tags = dict()
+	indexes = list()
+
+	for color in COLORS:
+		index = line_cpy.find(color)
+		while index != -1:
+			tags[str(index)] = color
+			indexes.append(index)
+			line_cpy = line_cpy.replace(color,SPECIAL_CHAR,1)
+			index = line_cpy.find(color)
+
+	#print(tags)
+	#print(indexes)
+	#print(line_cpy)
+
+	indexes.sort()
+
+	#print("indexes(sorted)=" + str(indexes))
+
+	line_wrapped = WRAPPER.fill(line_cpy)
+
+	for i in indexes:
+		line_wrapped = line_wrapped.replace(SPECIAL_CHAR,COLORS[tags[str(i)]], 1)
+
+	print(line_wrapped)
+
+
+
+
+
+#print("reconstructed:")
+#print(text)
+
+
+#text_spl = TEST.split(" ")
+#text_spl_cpy = list()
+
+
+
+
+
+#for word in text_spl:
+#	new_word = ""
+
+#	for color in COLORS:
+#		if word.find(color) != -1:
+#			new_word = word.replace(color, "")
+#			text_spl_cpy.append(color)
+#			break
+#		else:
+#			new_word = word
+		
+#	text_spl_cpy.append(new_word)
+
+#print(text_spl)
+#print(text_spl_cpy)
